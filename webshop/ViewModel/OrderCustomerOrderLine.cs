bef45826207dc520bbac18458920fffb98210bd9
Rotter <71013884+NordiAut@users.noreply.github.com>
2021-04-28 11:06:15 +0200

    Checkout V1
    Checkout VM
    Checkout Button in Cart

---
+++ webshop/ViewModel/OrderCustomerOrderLine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webshop.ViewModel
+{
+    public class OrderCustomerOrderLine
+    {
+        
+        public Nullable<int> Order_Id { get; set; }
+
+        public Nullable<int> Customer_Id { get; set; }
+        public Nullable<int> Customer_ID { get; set; }
+        public Nullable<decimal> PriceTotal { get; set; }
+        public Nullable<System.DateTime> DateOrdered { get; set; }
+
+        public string Email { get; set; }
+        public string Street { get; set; }
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public List<OrderLineProductViewModel> OrderLineProductList { set; get; }
+
+    }
+}
