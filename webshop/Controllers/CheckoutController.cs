dd1eb1aaff64eee77fba1b6ee3a77f07185f7474
Rotter <71013884+NordiAut@users.noreply.github.com>
2021-05-05 10:01:52 +0200

    EmailV1
    Email works
    
    TODO Attachement PDF

--- webshop/Controllers/CheckoutController.cs
+++ webshop/Controllers/CheckoutController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using Rotativa;
 using webshop.ViewModel;
 
 namespace webshop.Controllers
@@ -140,9 +144,31 @@ namespace webshop.Controllers
             
             // create invoice PDF
             var invoicePDF = new Rotativa.ActionAsPdf("InvoicePDF", orderObject);
+           
 
             //TODO Email
-            
+            string from = "oliver.rotter@gmx.at"; 
+            using (MailMessage mail = new MailMessage(from, "itn132163@qualifizierung.at"))
+            {
+                mail.Subject = "Invoice";
+                mail.Body = "Thanks for ordering!";
+
+                //Attachment attPDF = new Attachment(new MemoryStream(byteArray), "Invoice");
+                //mail.Attachments.Add(new Attachment(new MemoryStream(byteArray), "Invoice"));
+                //mail.Attachments.Add(attPDF);
+
+
+                mail.IsBodyHtml = false;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "mail.gmx.net";
+                smtp.EnableSsl = true;
+                NetworkCredential networkCredential = new NetworkCredential(from, "Oliver22!");
+                smtp.UseDefaultCredentials = true;
+                smtp.Credentials = networkCredential;
+                smtp.Port = 587;
+                smtp.Send(mail);
+                
+            }
 
             return invoicePDF;
             //return View(orderObject);
