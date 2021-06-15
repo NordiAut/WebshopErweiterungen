77762bc895ad1d0d50d84ae748ef0db2ee90120f
Rotter <71013884+NordiAut@users.noreply.github.com>
2021-06-15 18:40:54 +0200

    Invoice shows order
    Invoice now shows ordered products and price

--- webshop/Controllers/CheckoutController.cs
+++ webshop/Controllers/CheckoutController.cs
@@ -147,6 +147,86 @@ namespace webshop.Controllers
 
         public ActionResult Invoice(OrderCustomerOrderLine orderObject)
         {
+            var checkoutObject = new OrderCustomerOrderLine();
+
+            var customerId = Convert.ToInt32(Session["idUser"]);
+
+            // get customer
+            var customer = db.Customer.Where(x => x.Customer_ID == customerId).FirstOrDefault();
+            // get order from customer
+            var order = db.OrderTable.Where(x => x.Customer_ID == customerId).FirstOrDefault();
+            // get list of orderlines from order from customer
+            var orderId = order.Order_ID;
+            var orderLine = db.OrderLine.Where(o => o.Order_ID == orderId).ToList();
+            var cartlist = new List<OrderLineProductViewModel>();
+
+            decimal total = 0.0m;
+
+            bool isEmpty = !orderLine.Any();
+            if (isEmpty)
+            {
+                return RedirectToAction("EmptyCart", "Products");
+            }
+
+            // fill cartlist
+            foreach (var line in orderLine)
+            {
+
+                var product = db.Product.Where(x => x.Product_ID == line.Product_ID).FirstOrDefault();
+                // Fill up line
+                var temp_line = new OrderLineProductViewModel()
+                {
+                    ID = line.OrderLine_ID,
+                    Order_Id = line.Order_ID,
+                    Amount = line.Amount,
+                    NetUnitPrice = line.NetUnitPrice,
+                    TaxRate = line.TaxRate,
+                    Product_ID = line.Product_ID,
+                    Product_Name = product.Product_Name,
+                    ImagePath = product.ImagePath,
+                    Manufacturer_Name = product.Manufacturer.Manufacturer_Name,
+                    priceLine = line.Amount * line.NetUnitPrice
+
+                };
+                cartlist.Add(temp_line);
+
+                total += temp_line.priceLine ?? default;
+            }
+
+            // Netto and Brutto
+            var nettoTotal = total;
+
+
+            var bruttoTotal = nettoTotal * 1.2M;
+            var UST = bruttoTotal - nettoTotal;
+            ViewBag.UST = Math.Round(UST, 2);
+            //Total viewbag
+            bruttoTotal = Math.Round(bruttoTotal, 2);
+            ViewBag.Total = bruttoTotal;
+
+
+            //Fill checkout object
+            checkoutObject.Customer_Id = customer.Customer_ID;
+            checkoutObject.Order_Id = order.Order_ID;
+            checkoutObject.PriceTotal = total;
+            checkoutObject.Email = customer.Email;
+            checkoutObject.Street = order.Street;
+            checkoutObject.Zip = order.Zip.ToString();
+            checkoutObject.City = order.City;
+            checkoutObject.FirstName = order.FirstName;
+            checkoutObject.LastName = order.LastName;
+
+            //fill delivery-data 
+            checkoutObject.DeliveryStreet = order.Street;
+            checkoutObject.DeliveryZip = order.Zip.ToString();
+            checkoutObject.DeliveryCity = order.City;
+            checkoutObject.DeliveryFirstName = order.FirstName;
+            checkoutObject.DeliveryLastName = order.LastName;
+            //add cartlist to checkoutObject
+            checkoutObject.OrderLineProductList = cartlist;
+
+
+
             var invoice = new Rotativa.ActionAsPdf("InvoicePDF", orderObject);
             // create invoice PDF
             var invoicePDF = new Rotativa.ActionAsPdf("InvoicePDF", orderObject)
@@ -190,7 +270,7 @@ namespace webshop.Controllers
             }
 
             //return invoice;
-            return View(orderObject);
+            return View(checkoutObject);
         }
 
         public ActionResult InvoicePDF(OrderCustomerOrderLine orderObject)
